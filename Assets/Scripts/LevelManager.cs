38c7b98546f8ef171b0834a1e4404bbeb6044ea4
Solomon Yan <u1297631@gcloud.utah.edu>
2025-11-15 02:55:32 -0700

    Basic Yarn Spinner setup for VN prepared

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Yarn;
+using static Unity.Collections.Unicode;
 
 public class LevelManager : MonoBehaviour
 {
@@ -8,10 +10,38 @@ public class LevelManager : MonoBehaviour
     [Header("Spawn Settings")]
     public Vector3 startPosition = Vector3.zero;
 
+    public Yarn.Unity.DialogueRunner dialogueRunner;
+
     private int day = 0;
+
+    void Awake()
+    {
+        // Subscribe to the event
+        dialogueRunner.onDialogueComplete.AddListener(OnDialogueFinished);
+    }
+
+    private void Start()
+    {
+        dialogueRunner.StartDialogue("MainStory");
+    }
 
     void Update()
     {
+        /*
+        // If door slider is clicked on, start next dialogue (they also have needed to finish their daily tasks)
+        if ()
+        {
+            // TODO: Select dialogue from story (just the next yarn node)
+            dialogueRunner.StartDialogue("Start");
+        }
+        */
+
+        // Click anywhere to continue dialogue
+        if (dialogueRunner.IsDialogueRunning && Input.GetMouseButtonDown(0))
+        {
+            dialogueRunner.RequestNextLine();
+        }
+
         // Advance day when P is pressed (temp)
         if (Input.GetKeyDown(KeyCode.P))
         {
@@ -29,6 +59,13 @@ public class LevelManager : MonoBehaviour
                 spiderManager.SpawnClones(baseSpider, 1, spawnPos, Vector3.zero);
             }
         }
+    }
+
+    void OnDialogueFinished()
+    {
+        // TODO: Progress to the next day (restart everything like make new clones but keep the squished ones)
+        // Clones, daily reset, next yarn?
+        Debug.Log("The end");
     }
 
     public int GetCurrentDay()
