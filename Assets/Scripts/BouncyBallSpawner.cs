67f78700a9c8db52fcbfc28732bed6f4e4995cc3
Solomon Yan <u1297631@gcloud.utah.edu>
2025-11-22 23:52:39 -0700

    Increased requirement from 50 to 100 for ball and plane

--- Assets/Scripts/BouncyBallSpawner.cs
+++ Assets/Scripts/BouncyBallSpawner.cs
@@ -98,7 +98,7 @@ public class BouncyBallSpawner : MonoBehaviour
 
     public void CheckBallThrownCount()
     {
-        if (ballThrown >= 50 && LevelManager.getCurrentDay() > 4)
+        if (ballThrown >= 100 && LevelManager.getCurrentDay() > 4)
         {
             OnBallEndingReached?.Invoke();
         }
